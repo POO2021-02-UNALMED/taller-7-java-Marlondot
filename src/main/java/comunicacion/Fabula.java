cb8d597120bd7685f53644f38ea5ff28c8b098a7
Marlondot <80053997+Marlondot@users.noreply.github.com>
2021-12-06 20:38:36 -0500

    Update Fabula.java

--- src/main/java/comunicacion/Fabula.java
+++ src/main/java/comunicacion/Fabula.java
@@ -2,13 +2,13 @@ package comunicacion;
 
 public class Fabula extends Escrito{
 	
-	private String enseñanza;
+	private String ensenanza;
 	private String interpretacion;
 	
 	
-	public Fabula(String origen, String titulo, String autor, int paginas, String enseñanza, String interpretacion) {
+	public Fabula(String origen, String titulo, String autor, int paginas, String ensenanza, String interpretacion) {
 		super(origen, titulo, autor, paginas);
-		this.enseñanza = enseñanza;
+		this.ensenanza = ensenanza;
 		this.interpretacion = interpretacion;
 	}
 	
@@ -30,18 +30,18 @@ public class Fabula extends Escrito{
 		for(Object field: fields) {
 			answer +=  field.toString() + "\n";
 		}*/
-		answer+= this.getEnseñanza(); 
+		answer+= this.getEnsenanza(); 
 		return answer;
 	}
 
 	//
 	//methods
 	//
-	public String getEnseñanza() {
-		return enseñanza;
+	public String getEnsenanza() {
+		return ensenanza;
 	}
-	public void setEnseñanza(String enseñanza) {
-		this.enseñanza = enseñanza;
+	public void setEnsenanza(String ensenanza) {
+		this.ensenanza = ensenanza;
 	}
 
 	public String getInterpretacion() {
